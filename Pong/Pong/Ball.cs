8d0c5b3d6b815c86efbbe2b42c49ced1e4acf56a
JohnyBoardProfi <91531366+JohnyBoardProfi@users.noreply.github.com>
2021-09-29 15:30:44 +0300

    Add files via upload

---
+++ Pong/Pong/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Pong
+{
+    class Ball
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        int rX, rY, boardHeight, boardWidth;
+        Printer p = new Printer();
+
+        public int Direction { get; set; }
+
+        public Ball(int x, int y, int boardHeight, int boardWidth)
+        {
+            X = x;
+            Y = y;
+            this.boardHeight = boardHeight;
+            this.boardWidth = boardWidth;
+            rX = -1;
+            rY = 1;
+        }
+
+        public void Rules(Paddle p1, Paddle p2)
+        {
+            p.Print(X, Y, "\0");
+            if (Y <= 1 || Y >= boardHeight)
+            {
+                rY *= -1;
+            }
+            if (((X == 3 || X == boardWidth - 3) && (p1.Y - (p1.Length / 2)) <= Y && (p1.Y + (p1.Length / 2)) > Y))
+            {
+                rX *= -1;
+                if (Y == p1.Y)
+                {
+                    Direction = 0;
+                }
+                if ((Y >= (p1.Y - (p1.Length / 2)) && Y < p1.Y) || (Y > p1.Y && Y < (p1.Y + (p1.Length / 2))))
+                {
+                    Direction = 1;
+                }
+            }
+            switch(Direction)
+            {
+                case 0:
+                    X += rX;
+                    break;
+                case 1:
+                    X += rX;
+                    Y += rY;
+                    break;
+            }
+        }
+
+        public void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            p.Print(X, Y, "■");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Pong/Pong/Board.cs
@@ -0,0 +1,47 @@
+﻿namespace Pong
+{
+    public class Board
+    {
+        Printer p = new Printer();
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public Board()
+        {
+            Height = 20;
+            Width = 60;
+        }
+
+        public Board(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Print()
+        {
+            for (int i = 1; i <= Width; i++)
+            {
+                p.Print(i, 0, "-");
+            }
+            for (int i = 1; i <= Width; i++)
+            {
+                p.Print(i, Height + 1, "-");
+            }
+            for (int i = 1; i <= Height; i++)
+            {
+                p.Print(0, i, "|");
+            }
+            for (int i = 1; i <= Height; i++)
+            {
+                p.Print(Width + 1, i, "|");
+            }
+            p.Print(0, 0, "┌");
+            p.Print(Width + 1, 0, "┐");
+            p.Print(0, Height + 1, "└");
+            p.Print(Width + 1, Height + 1, "┘");
+        }
+    }
+}
